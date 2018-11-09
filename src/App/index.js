0dcdef1e53403ebe44196cc1bdb28e9e464891de
independnt <gonzalezdennis6@gmail.com>
2018-11-09 14:19:43 -0500

    add overall padding with AppLayout

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import WelcomeMessage from './WelcomeMessage'
 import './App.css';
-import styled, {css} from 'styled-components'
+import AppLayout from './AppLayout'
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <AppLayout>
       <WelcomeMessage/>
-      </div>
+      </AppLayout>
     );
   }
 }
