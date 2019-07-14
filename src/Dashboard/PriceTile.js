6c118c923c9494d6ff76f3525e8c7e78d3e1a496
independnt <gonzalezdennis6@gmail.com>
2019-07-14 19:15:08 -0400

    finished dashboard pricetile styling

--- src/Dashboard/PriceTile.js
+++ src/Dashboard/PriceTile.js
@@ -4,7 +4,21 @@ import {SelectableTile} from "../Shared/Tile"
 import {fontSize3, fontSizeBig} from "../Shared/Style"
 import {CoinHeaderGridStyled} from "../Settings/CoinHeaderGrid"
 
-const justifyRight = styled.div`
+const TickerPrice = styled.div`
+  ${fontSizeBig}
+`
+const ChangePct = styled.div`
+  color: green;
+  ${props => props.red && css`
+      color: red;
+  `}
+`
+
+const JustifyLeft = styled.div`
+  justify-self: left;
+`
+
+const JustifyRight = styled.div`
   justify-self: right;
 `
 
@@ -14,16 +28,46 @@ const numberFormat = number => {
 
 const PriceTileStyled = styled(SelectableTile)`
   ${props => props.compact && css`
+    display: grid;
     ${fontSize3}
+    grid-gap: 5px;
+    grid-template-columns: repeat(3, 1fr);
+    justify-items: right;
   `}
 `
+
+function ChangePercent({data}){
+  return (
+    <JustifyRight>
+      <ChangePct red={data.CHANGEPCT24HOUR < 0}>
+        {numberFormat(data.CHANGEPCT24HOUR)}
+      </ChangePct>
+    </JustifyRight>
+  )
+}
+
 function PriceTile({sym, data}){
   return (
     <PriceTileStyled>
       <CoinHeaderGridStyled>
         <div> {sym} </div>
-        <justifyRight> {numberFormat(data.CHANGEPCT24HOUR)}</justifyRight>
+        <ChangePercent data={data}/>
       </CoinHeaderGridStyled>
+      <TickerPrice>
+        ${numberFormat(data.PRICE)}
+      </TickerPrice>
+    </PriceTileStyled>
+  )
+}
+
+function PriceTileCompact({sym, data}){
+  return (
+    <PriceTileStyled compact>
+        <JustifyLeft> {sym} </JustifyLeft>
+        <ChangePercent data={data}/>
+      <div>
+        ${numberFormat(data.PRICE)}
+      </div>
     </PriceTileStyled>
   )
 }
@@ -32,9 +76,10 @@ function PriceTile({sym, data}){
 export default function({price, index}){
   let sym = Object.keys(price)[0];
   let data = price[sym]['USD'];
+  let TileClass = index < 5 ? PriceTile : PriceTileCompact
   return (
-    <PriceTile sym={sym} data={data}>
+    <TileClass sym={sym} data={data}>
       {sym} {data.PRICE}
-    </PriceTile>
+    </TileClass>
   )
 }
